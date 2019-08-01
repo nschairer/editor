e90c80ec92a9c78e21f87873c5d6a45eb332a445
Noah Schairer <nschairer@gmail.com>
2019-07-31 21:53:22 -0500

    initial

---
+++ public/lexer.js
@@ -0,0 +1,43 @@
+
+//never match globally, want the first match
+const tokens_types = [
+    { regex: /^(select|where|is|from|group|by|having|like|and|between|case|when|then|end|else|create|delete|alter|table|insert|into|values|update|while|if|in)(?!([^\s\(\)\[\]\*\-\/]))/i, type: 'KEYWORD'},
+    { regex: /(--(.*)[\n\r]?|\/\*((.|\n)*)\*\/)/, type: 'COMMENT'},
+    { regex: /^(\*|=|\+|-|<|>)/, type: 'OPERATOR'},
+    { regex: /^\s+/, type: 'SPACE'},
+    { regex: /^(\(|\))/, type: 'PT'},
+    { regex: /^(\[|\])/, type: 'SQBRACKET'},
+    { regex: /^([^\s]+)(?![\(|\)])/,  type: 'USERDEFINED'}
+]
+
+
+
+export default function lexer(input_string) {
+    let temp = input_string
+    let tokens = []
+    let isMatch = true
+    while(temp.length && isMatch) {
+        let match;
+        for(let t of tokens_types) {
+            match = temp.match(t.regex)
+            if(match) {
+                tokens.push(
+                    {
+                        type: t.type,
+                        value: match[0]
+                    }
+                )
+                temp = temp.substring((match.index+match[0].length))
+                break;
+            }
+        }
+        if (match) {
+             match = null
+             continue
+        }
+        tokens.push({type: 'NOTTRACKED', value: temp})
+        isMatch = false
+   }
+   console.log(tokens)
+   return tokens
+}
---
+++ public/parser.js
@@ -0,0 +1,31 @@
+import lexer from './lexer.js'
+const colors = {
+    KEYWORD: 'yellow',
+    OPERATOR: 'orange',
+    COMMENT: 'green',
+    USERDEFINED: 'white'
+}
+
+export default function parse(input) {
+    const tokens = lexer(input)
+    let html = ''
+    for (let token of tokens) {
+        if(colors[token.type]) {
+            html += `<span style="color:${colors[token.type]}">${token.value}</span>`
+        } else {
+            html += token.value
+        }
+    }
+    console.log(html)
+    return html
+}
+
+const editor = document.getElementById('editor')
+editor.addEventListener('input', e => {
+    e.preventDefault()
+    const innertext = editor.innerText
+    editor.innerHTML = parse(innertext)
+    const range = window.getSelection()
+    range.selectAllChildren(editor)
+    range.collapseToEnd()
+})
